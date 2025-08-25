b630eea687399b77b2e1ec38e67868f79da8ad31
Luis Prendas <luisprendas.dev@gmail.com>
2025-08-25 13:03:32 -0600

    refactor: remove socket.io related functionality and associated middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,8 @@
 import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
 import { PORT } from './config/env';
 import app from './app';
-import { socketAuth } from './middlewares/socketAuth';
-import { handleRoomEvents } from './sockets/roomHandler';
-import handleWebRTCEvents from './sockets/webrtcHandler';
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: { origin: '*' } });
-
-io.use(socketAuth);
-
-io.on('connection', (socket: Socket) => {
-  console.log(`⚡ Nuevo cliente conectado: ${socket.id}`);
-  handleRoomEvents(io, socket);
-  handleWebRTCEvents(io, socket);
-});
 
 httpServer.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
